b9ebd3a245050f481d045469bb20ebd189740f3f
Abiodun Osagie <abiodunosagie8@gmail>
2025-03-31 15:57:01 +0100

    Final feature added

---
+++ Notes Watch App/Component/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Notes Watch App
+//
+//  Created by Abiodun Osagie on 31/03/2025.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
---
+++ Notes Watch App/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Notes Watch App
+//
+//  Created by Abiodun Osagie on 31/03/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
